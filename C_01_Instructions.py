de74ca56d07e48cde2148e36dedb013b3121e5fe
Zach-Stuart <zachstuart@mmc.school.nz>
2024-05-15 13:04:01 +1200

    soemthing soemngunfoipkfmp

--- C_01_Instructions.py
+++ C_01_Instructions.py
@@ -1,13 +1,47 @@
-print("Roll It 13")
+# check users enter yes or no
+def yes_no(question):
+    while True:
+        response = input(question).lower()
 
-#loop for testing
+        # checks user response, question
+        # repeats if users don't enter yes or no
+        if response == "yes" or response == "y":
+            return "yes"
+        elif response == "no" or response == "n":
+            return "no"
+        else:
+            print("You did not type a valid response.")
 
-while True:
-    want_instructions = input("Do you want to read the instructions? ").lower()
 
-    if want_instructions == "yes" or want_instructions == "y":
-        print("You chose 'Yes'.")
-    elif want_instructions == "no" or want_instructions == "n":
-        print("You chose 'No'.")
-    else:
-        print("You did not choose a valid response.")
+def instruction():
+    print('''
+    **** Instructions ****
+    
+    When life gives you lemons, don't make lemonade
+    Make life take the lemons back
+    Get mad
+    I don't want your damn lemons, what am I supposed to do with these?
+    Demand to see life's manager
+    Make life rue the day it thought it could give Cave Johnson lemons
+    Do you know who I am?
+    I'm the man who's gonna burn your house down!
+    With the lemons!
+    I'm gonna get my engineers to invent a combustible lemon that burns your house down!
+    
+    
+    ''')
+
+
+# main routine
+print()
+print('Roll It 13')
+print()
+
+#   loop for testing
+
+want_instructions = yes_no("Do you want to read the instructions? ")
+
+if want_instructions == "yes":
+    instruction()
+
+print("program")
