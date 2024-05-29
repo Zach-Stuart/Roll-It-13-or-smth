919e00fd0d139b7a67ff5740381e0539f839adcf
Zach-Stuart <zachstuart@mmc.school.nz>
2024-05-29 13:00:16 +1200

    Added random AI and comparison code

--- 20-RPS/C_01_Instructions.py
+++ 20-RPS/C_01_Instructions.py
@@ -1,32 +1,34 @@
-# check users enter yes or no
-def yes_no(question):
-    while True:
-        response = input(question).lower()
+# Check that users have entered a valid
+# option based on a list
+def string_checker(question, valid_ans=("yes", "no")):
 
-        # checks user response, question
-        # repeats if users don't enter yes or no
-        if response == "yes" or response == "y":
-            return "yes"
-        elif response == "no" or response == "n":
-            return "no"
-        else:
-            print("You did not type a valid response.")
+    error = f"Please enter a valid response from the following list: {valid_ans}"
+
+    while True:
+
+        # make sure user response is lowercase
+        user_response = input(question).lower()
+        for item in valid_ans:
+            # check if user response is a word in the list
+            if item == user_response:
+                return item
+
+            # check if user response is same as first letter of an item in list
+            elif user_response == item[0]:
+                return item
+
+        # print error if user enters invalid response
+        print(error)
+        print()
 
 
 def instruction():
     print('''
     **** Instructions ****
     
-    When life gives you lemons, don't make lemonade
-    Make life take the lemons back
-    Get mad
-    I don't want your damn lemons, what am I supposed to do with these?
-    Demand to see life's manager
-    Make life rue the day it thought it could give Cave Johnson lemons
-    Do you know who I am?
-    I'm the man who's gonna burn your house down!
-    With the lemons!
-    I'm gonna get my engineers to invent a combustible lemon that burns your house down!
+    Pick between Rock, Paper and Scissors
+    Do I really need to explain this to you
+    Who hasn't played Rock, Paper, Scissors at some point in their life
     
     
     ''')
@@ -39,7 +41,7 @@ print()
 
 #   loop for testing
 
-want_instructions = yes_no("Do you want to read the instructions? ")
+want_instructions = string_checker("Do you want to read the instructions? ")
 
 if want_instructions == "yes":
     instruction()
---
+++ 20-RPS/C_04_computer_choice.py
@@ -0,0 +1,8 @@
+import random
+
+rps_list = ["rock", "paper", "scissors", "xxx"]
+
+for item in range(0, 30):
+
+
+    print(comp_choice, end="\t")
